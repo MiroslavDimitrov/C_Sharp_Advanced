4b9acbca15d5935bbd86e5ba6d1f681613c42eaa
Miroslav Todorov <proudenough@gmail.com>
2022-08-09 18:00:14 +0300

    Add files via upload

---
+++ Mth/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace C_Sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //celling to the next higher number
+            Console.WriteLine("Celling: " + Math.Ceiling(15.3));
+            //celling without the decimal part of the current number
+            Console.WriteLine("Floor: " + Math.Floor(15.3));
+
+            int numberOne = 25;
+            int numberTwo = 50;
+
+            Console.WriteLine("Lower of number one: {0} and number two: {1} is {2}", numberOne, numberTwo, Math.Min(numberOne, numberTwo));
+            Console.WriteLine("Higher of number one: {0} and number two: {1} is {2}", numberOne, numberTwo, Math.Max(numberOne, numberTwo));
+
+            Console.WriteLine("Cos of 1 is: {0}", Math.Cos(1));
+            //calculate the current number multiply the result by the number (power)
+            Console.WriteLine("3 to the power of 5 is {0}", Math.Pow(3, 5));
+        }
+    }
+}
---
+++ Nullable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C_sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? numberOne = null;
+            int? numberTwo = 2550;
+
+            double? numberThree = new Double?();
+            double? numberFour = 3.14157;
+
+            bool? boolaVlue = new bool?();
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", numberOne, numberTwo, numberThree, numberFour, boolaVlue);
+        }
+    }
+}
