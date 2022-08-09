fda04836f0e412bc9369bd2868c636694396d5eb
Miroslav Todorov <proudenough@gmail.com>
2022-08-09 18:05:59 +0300

    Add files via upload

---
+++ ReadFromTextFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace C_sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\miro\Desktop\Text files\NewTextDocument.txt"); 
+            Console.WriteLine("The text from the file is: {0}", text);
+
+            string[] lines = File.ReadAllLines(@"C:\Users\miro\Desktop\C_Sharp Advanced.txt");
+
+            Console.WriteLine("Content of the text file is: ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+        }
+    }
+}
---
+++ RegEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace C_sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathern = @"\d";
+            Regex regex = new Regex(pathern);
+            string text = "My name is Miroslav, and my number is 12345.";
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found:\n {1}", matches.Count, text);
+
+            foreach (Match hit in matches)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+}
