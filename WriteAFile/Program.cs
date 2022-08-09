965b912aaf94dc2cbd248c2357a58d3ea4bb0321
Miroslav Todorov <proudenough@gmail.com>
2022-08-09 18:11:40 +0300

    Add files via upload

---
+++ WriteAFile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace C_sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines =
+            { 
+                "first line --> Miroslav",
+                "Second line --> Dimitrov",
+                "Third line --> from Lovech"
+            };
+
+            string[] numbers =
+            { 
+                "2",
+                "3",
+                "4",
+            };
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\miro\Desktop\Text files\NewTextPartTwo.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    if (line.Contains("Lovech"))
+                    {
+                        file.WriteLine("Lives in Sofia");
+                    }
+                }
+            }
+
+            //create a new file
+            File.WriteAllLines(@"C:\Users\miro\Desktop\Text files\NewText.txt", numbers);
+            //write in an existing file
+            File.WriteAllLines(@"C:\Users\miro\Desktop\Text files\NewTextDocument.txt", lines);
+        }
+    }
+}
