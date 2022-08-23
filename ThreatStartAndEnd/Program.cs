c3625aa8067c755cf54a4380caef0ee569a68aa6
Miroslav Todorov <proudenough@gmail.com>
2022-08-23 10:15:26 +0300

    Add files via upload

---
+++ ThreatStartAndEnd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_sharp_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var taskCompletiionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(3000);
+                taskCompletiionSource.TrySetResult(true);
+            });
+
+            Console.WriteLine($"{thread.ManagedThreadId}");
+            thread.Start();
+            var task = taskCompletiionSource.Task.Result;
+            Console.WriteLine("{0}", task);
+        }
+    }
+}
